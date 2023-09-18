8f8d7499c274e7ab0b4b7665b1d80c8361f6a1cc
Johnny Chau <jhnyckc@gmail.com>
2023-09-17 21:16:57 -0700

    Error handling for camera

--- pigui/components/camera.py
+++ pigui/components/camera.py
@@ -1,6 +1,7 @@
 from pigui.hardware.camera import Camera, ImageProcessor
 from pigui.hardware.controller import Button, Joystick
 from pigui.ui.ui import Component, Document, EventListener
+from pigui.components.still import Still
 import time
 import os
 
@@ -10,37 +11,64 @@ class CameraApp(Component):
         super().__init__(document)
         self.camera = None
         self.image_directory = image_directory
+        self.is_capturing_image = False
+
         # Return to menu page
-        return_listener = EventListener([(self.document.controller.joystick.on_up, self.close_cam)], sleep=0.1)
+        return_listener = EventListener(
+            [(self.document.controller.joystick.on_up, self.close_cam)],
+            sleep=0.1,
+            debounce=0.5,
+        )
         # Capture image
-        capture_listener = EventListener([(self.document.controller.joystick.on_press, self.capture_img)], sleep=0.1)
+        capture_listener = EventListener(
+            [(self.document.controller.joystick.on_press, self.capture_img)],
+            sleep=0.1,
+            debounce=0.5,
+        )
         self.register_event_listener([return_listener, capture_listener])
-        
-            
+
     def close_cam(self):
         # Return to menu page
         self.document.goto_frame("menu")
-        
-        # Turn off camera 
+
+        # Turn off camera
         if self.camera:
-            self.camera.camera.close()
+            self.camera.close()
             self.camera = None
             print("Camera closed.")
-        
+
     def capture_img(self):
-        img_name = self.get_image_name()
-        self.camera.capture_image(img_name)
-        print(f"Image {img_name} saved!")
-        
+        if self.camera and not self.is_capturing_image:
+            self.is_capturing_image = True
+
+            img_name = self.get_image_name()
+            self.camera.capture_image(img_name)
+            print(f"Image {img_name} saved!")
+
+            self.is_capturing_image = False
+
     def render(self):
         if self.camera is None:
             self.camera = Camera()
-        for frame in self.camera.capture_video_stream():
-            return ImageProcessor.load_img(frame.array, resize=(128,64))
-        
+
+        # Taking image
+        if self.is_capturing_image:
+            return Still.text("Saving image.....")
+
+        # Previewing
+        try:
+            frame = next(self.camera.video_stream())
+            return ImageProcessor.load_img(frame.array)
+        except:
+            return None
+
     def get_image_name(self, prefix="IMG"):
         image_files = os.listdir(self.image_directory)
         image_files = [f for f in image_files if f.startswith(prefix)]
-        image_num = 0 if not image_files else max([int(f.split(prefix)[1].split(".jpg")[0]) for f in image_files])        
+        image_num = (
+            0
+            if not image_files
+            else max([int(f.split(prefix)[1].split(".jpg")[0]) for f in image_files])
+        )
         img_name = f"{prefix}{image_num + 1}.jpg"
         return img_name
--- pigui/hardware/camera.py
+++ pigui/hardware/camera.py
@@ -5,43 +5,53 @@ from io import BytesIO
 from typing import Union, Generator
 from PIL import Image, ImageFont
 from numpy import ndarray
+import sys
+
 
 class Camera:
-    def __init__(self):
+    def __init__(self, image_resolution=(1920, 1080)):
         self.camera = picamera.PiCamera()
         self.camera.resolution = (WIDTH, HEIGHT)
+        self.image_resolution = image_resolution
         self.camera.framerate = FRAMERATE
-    
-    def capture_img_stream(self) -> BytesIO:
+
+    def capture_img_stream(self, format="jpeg") -> BytesIO:
         img_stream = BytesIO()
-        self.camera.capture(img_stream, format="jpeg")
+        self.camera.capture(img_stream, format=format)
         return img_stream
-    
-    def capture_image(self, path="PI_IMG.jpeg"):
-        self.camera.capture(path, format="jpeg")
-        
-    def capture_video_stream(self) -> Generator:
+
+    def capture_image(self, path="PI_IMG.jpeg", format="jpeg"):
+        # Update camera resolution when taking photo
+        self.camera.resolution = self.image_resolution
+        self.camera.capture(path, format=format)
+        # Revert camera resolution optimized for video/preview mode when done
+        self.camera.resolution = (WIDTH, HEIGHT)
+
+    def video_stream(self, format="bgr") -> Generator:
         vid_stream = PiRGBArray(self.camera)
-        for frame in self.camera.capture_continuous(vid_stream, format="bgr", use_video_port=True):
+        for frame in self.camera.capture_continuous(
+            vid_stream, format=format, use_video_port=True
+        ):
             yield frame
             # Clear buffer before next frame
             vid_stream.truncate(0)
+            
+    def close(self):
+        self.camera.close()
 
 
 class ImageProcessor:
-    
     @staticmethod
     def load_img(img: Union[str, BytesIO, ndarray], resize=None) -> Image:
         if isinstance(img, str) or isinstance(img, BytesIO):
-            image = Image.open(img).convert('1')
+            image = Image.open(img).convert("1")
         elif isinstance(img, ndarray):
-            image = Image.fromarray(img).convert('1')
+            image = Image.fromarray(img).convert("1")
         if resize:
-            return image.resize(resize, Image.ANTIALIAS).convert('1')
+            return image.resize(resize, Image.ANTIALIAS).convert("1")
         return image
-    
+
     @staticmethod
     def convert_ppm(img: Union[str, BytesIO]):
-        img = Image.open(img).resize((128, 64), Image.ANTIALIAS).convert('1')
+        img = Image.open(img).resize((128, 64), Image.ANTIALIAS).convert("1")
         return img
-    
