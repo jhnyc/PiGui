47ffda11b29fb80f4b92caee0afea2e8066642f7
Johnny Chau <jhnyckc@gmail.com>
2023-09-19 17:47:59 -0700

    added func to measure fps

--- pigui/utils/testing.py
+++ pigui/utils/testing.py
@@ -1,4 +1,6 @@
 import time
+from typing import Union
+from pigui.ui.ui import Component, Frame, Document
 
 
 def measure_event_time(event_func):
@@ -16,3 +18,19 @@ def measure_event_time(event_func):
             reaction_time = end_time - start_time
             print(reaction_time)
             return reaction_time
+
+
+def measure_fps(object: Union[Component, Frame, Document], sleep=0):
+    frames = 0
+    start_time = time.time()
+    while True:
+        object.render()
+        frames += 1
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= 1.0:
+            fps = frames / elapsed_time
+            print(f"FPS: {fps:.2f}")
+            frames = 0
+            start_time = time.time()
+        if sleep:
+            time.sleep(sleep)
